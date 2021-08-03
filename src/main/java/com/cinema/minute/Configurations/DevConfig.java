82dbf25ef23022cde3e2d43da3c65a1bf953cfda
oueslati00 <boueslati76@gmail.com>
2021-08-03 07:15:47 -0700

    auth module was integrated in front-end note : add bad request to forget password rest controller

--- src/main/java/com/cinema/minute/Configurations/DevConfig.java
+++ src/main/java/com/cinema/minute/Configurations/DevConfig.java
@@ -1,8 +1,9 @@
 package com.cinema.minute.Configurations;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class DevConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
